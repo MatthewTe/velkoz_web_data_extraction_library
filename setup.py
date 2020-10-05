593a16a9956b06e8c6ba6ff8731cdb65ebd63cac
MatthewTe <teelucksingh.matthew1@gmail.com>
2020-10-05 07:46:47 -0400

    Travis CI config commit

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='velkoz_data_pipeline',
       version='0.0.1',
       description='The private testing installation for the Velkoz Data Pipeline Library',
       license='...',
-      packages=['velkoz_web_packages'],
+      packages=find_packages(),
       zip_safe=False)
