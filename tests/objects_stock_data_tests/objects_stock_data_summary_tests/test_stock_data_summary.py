075e4442a28322d145f24e1efc67182c5946f481
MatthewTe <test@example.com>
2020-11-07 20:54:40 -0500

    Test Framework

---
+++ tests/objects_stock_data_tests/objects_stock_data_summary_tests/test_stock_data_summary.py
@@ -0,0 +1,26 @@
+# Importing testing frameworks:
+import unittest
+
+# Importing 3rd party packages:
+import bs4
+import sqlite3
+import sqlalchemy
+import pandas as pd
+import numpy as np
+import datetime
+
+# Importing Velkoz Packages for testing:
+from velkoz_web_packages.objects_stock_data.objects_stock_db_summary.ingestion_engines_stock_data_summary import StockDataSummaryIngestionEngine
+from velkoz_web_packages.objects_stock_data.objects_stock_price.web_objects_stock_price import NASDAQStockPriceResponseObject
+from velkoz_web_packages.objects_stock_data.objects_stock_price.ingestion_engines_stock_price import StockPriceDataIngestionEngine
+from velkoz_web_packages.objects_stock_data.objects_fund_holdings.web_objects_fund_holdings import NASDAQFundHoldingsResponseObject
+from velkoz_web_packages.objects_stock_data.objects_fund_holdings.ingestion_engines_fund_holdings import FundHoldingsDataIngestionEngine
+
+# Declaring global parameters for use in testing class:
+
+class StockDataSummaryIngestionEngineTest(unittest.TestCase):
+
+    def test_base_ingestion_engine_initalization(self):
+        """
+        """
+        pass
